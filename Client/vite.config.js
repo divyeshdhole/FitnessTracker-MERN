20df2b7d6462a979ad3ce470fe3dbe0e1e87f34c
divyeshdhole <divyeshlic2003@gmail.com>
2024-12-02 13:37:57 +0530

    Fix Vercel build configuration

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Handles SPA routing for client-side navigation
+  },
   build: {
-    outDir: 'dist',  // This ensures the output is in the 'dist' folder
-  }
+    rollupOptions: {
+      input: {
+        main: './index.html', // Specify the main entry point
+      },
+      outDir: 'dist'
+
+    },
+  },
+
 });
